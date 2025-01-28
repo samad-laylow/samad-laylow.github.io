47e72e91e2b6592f78831234665690472b60f857
samad-laylow <doha.dohaajja@gmail.com>
2025-01-28 11:24:33 +0100

    Update script.js for PWA and service worker

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ window.addEventListener("keydown", function(event) {
   }
 
 });
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker enregistré avec succès:", registration);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de l'enregistrement du Service Worker:", error);
+      });
+  });
+}
+
