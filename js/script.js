b323cc707ee44d00d69bd21083725df8328ed67d
samad-laylow <doha.dohaajja@gmail.com>
2025-01-28 11:50:06 +0100

    Add GA to script.js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,18 @@ if ("serviceWorker" in navigator) {
   });
 }
 
+
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-410HY4E7NS';
+  script.async = true;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-410HY4E7NS');
+})();
+
+
+
